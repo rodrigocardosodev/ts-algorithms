b00a7b0fb61c49660bfc75af0dcd96360e3ba8f7
Rodrigo Cardoso <rodrigocardoso@Rodrigos-MacBook-Air.local>
2023-05-02 20:30:43 -0300

    feat: shell-sort algorithm

---
+++ src/sorting-algorithms/shell-sort.ts
@@ -0,0 +1,27 @@
+function shellSort(input: string): string {
+  let arr = input.split('');
+  let n = arr.length;
+
+  // Comece com um intervalo grande, depois reduza
+  for (let gap = Math.floor(n / 2); gap > 0; gap = Math.floor(gap / 2)) {
+      // Faça uma inserção ordenada para este intervalo (ou lacuna)
+      for (let i = gap; i < n; i++) {
+          let temp = arr[i];
+          let j;
+          for (j = i; j >= gap && arr[j - gap] > temp; j -= gap) {
+              arr[j] = arr[j - gap];
+          }
+          arr[j] = temp;
+      }
+  }
+
+  return arr.join('');
+}
+
+// Testando o algoritmo
+const runShellSort = (unsortedString: string) => {
+  const sortedString = shellSort(unsortedString);
+  console.log(sortedString);
+}
+
+runShellSort("dcbaehgf");
