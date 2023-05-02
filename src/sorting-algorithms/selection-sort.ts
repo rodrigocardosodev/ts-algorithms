79cfc3357ce558657f8419795e76deaa97442414
Rodrigo Cardoso <rodrigocardoso@Rodrigos-MacBook-Air.local>
2023-05-02 20:30:34 -0300

    feat: selection-sort algorithm

---
+++ src/sorting-algorithms/selection-sort.ts
@@ -0,0 +1,29 @@
+function selectionSort(input: string): string {
+  let arr = input.split('');
+  let n = arr.length;
+
+  for (let i = 0; i < n; i++) {
+      // Encontre o mínimo no array desordenado
+      let minIdx = i;
+      for (let j = i + 1; j < n; j++) {
+          if (arr[j] < arr[minIdx]) {
+              minIdx = j;
+          }
+      }
+
+      // Troque o elemento mínimo encontrado com o primeiro elemento
+      let temp = arr[i];
+      arr[i] = arr[minIdx];
+      arr[minIdx] = temp;
+  }
+
+  return arr.join('');
+}
+
+// Testando o algoritmo
+const runSelectionSort = (unsortedString: string) => {
+  const sortedString = selectionSort(unsortedString);
+  console.log(sortedString);
+}
+
+runShellSort("dcbaehgf");
