3680f399f7c5b557a8f1ac73a0a06f27b421b96c
Rodrigo Cardoso <rodrigocardoso@Rodrigos-MacBook-Air.local>
2023-05-02 20:30:03 -0300

    feat: insertion-sort algorithm

---
+++ src/sorting-algorithms/insertion-sort.ts
@@ -0,0 +1,26 @@
+function insertionSort(input: string): string {
+  let arr = input.split('');
+  let n = arr.length;
+
+  for (let i = 1; i < n; i++) {
+      let key = arr[i];
+      let j = i - 1;
+
+      // Mova os elementos do arr[0...i-1], que são maiores que a chave, para uma posição à frente de sua posição atual
+      while (j >= 0 && arr[j] > key) {
+          arr[j + 1] = arr[j];
+          j = j - 1;
+      }
+      arr[j + 1] = key;
+  }
+
+  return arr.join('');
+}
+
+// Testando o algoritmo
+const runInsertionSort = (unsortedString: string) => {
+  const sortedString = insertionSort(unsortedString);
+  console.log(sortedString);
+}
+
+runInsertionSort("dcbaehgf");
