9f210fa3459f22adcdc6c82e33a40a1e6b4d559b
Rodrigo Cardoso <rodrigocardoso@Rodrigos-MacBook-Air.local>
2023-05-02 22:37:55 -0300

    feat: graph bfs algorithm

---
+++ src/graph-algorithms/shortest-path/bfs-algorithm.ts
@@ -0,0 +1,32 @@
+import { graph, Graph } from "./graph";
+
+function bfs(graph: Graph, start: number, end: number): number[] {
+    let queue: number[][] = [[start]];
+    let visited = new Set();
+
+    while (queue.length > 0) {
+        let path = queue.shift() as number[];
+        let node = path[path.length - 1];
+
+        if (node === end) {
+            return path;
+        }
+
+        if (!visited.has(node)) {
+            visited.add(node);
+
+            for (let i = 0; i < graph[node].length; i++) {
+                if (graph[node][i] === 1) {
+                    let newPath = [...path];
+                    newPath.push(i);
+                    queue.push(newPath);
+                }
+            }
+        }
+    }
+
+    return [];
+}
+
+
+console.log(bfs(graph, 0, 4));  // Output: [ 0, 2, 4 ]
